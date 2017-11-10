04d8b5320c5d096dcaeb752478a03c6e7799e97e
Erick Lui <erickjin.lui@gmail.com>
2017-11-09 17:22:51 -0800

    added mergeSortPrac.js

--- sorting/mergeSortPrac.js
+++ sorting/mergeSortPrac.js
@@ -1,35 +1,31 @@
-/*
-1) Mergesort left half of array
-2) Mergesort right half of array
-3) Merge left and right half in sorted order
-*/
-
 function mergeSort(arr) {
+  // console.log('newArr', arr);
   if (arr.length < 2) return arr;
   let middle = Math.floor(arr.length / 2);
   let left = arr.slice(0, middle);
   let right = arr.slice(middle, arr.length);
 
   return merge(mergeSort(left), mergeSort(right));
+
 }
 
 function merge(left, right) {
+  console.log(`merging ${left} ${right}`);
   let result = [];
 
   while (left.length && right.length) {
-    console.log(left, right, 'result', result);
     if (left[0] <= right[0]) {
       result.push(left.shift());
     } else {
       result.push(right.shift());
     }
   }
-
   while (left.length) result.push(left.shift());
   while (right.length) result.push(right.shift());
-  console.log(result, left, right);
+  console.log('result:', result);
   return result;
 }
 
-const arr = [10,9,5,1,2,4,3,7,8];
+
+const arr = [10,9,5,1,2,4,7,8];
 console.log(mergeSort(arr));

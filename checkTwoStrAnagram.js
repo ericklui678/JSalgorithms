eba7192d2e82221cb0b3896254893d57b93dd4cc
Erick Lui <erickjin.lui@gmail.com>
2017-11-30 14:30:23 -0800

    added dictionaries for checkTwoStrAnagram.js

--- checkTwoStrAnagram.js
+++ checkTwoStrAnagram.js
@@ -5,22 +5,37 @@ Given two strings, check if a string is an anagram of the other string
 */
 
 const checkAnagramOf = (str1, str2) => {
-  const letterMap = new Map();
+  // using Maps
+
+  // const letterMap = new Map();
+  //
+  // for (let char of str1) {
+  //   letterMap.set(char, letterMap.get(char) + 1 || 1);
+  // }
+  //
+  // for (let char of str2) {
+  //   if (!letterMap.has(char)) return false;
+  //
+  //   let charVal = letterMap.get(char);
+  //   if (charVal === 1) letterMap.delete(char);
+  //   else letterMap.set(char, charVal - 1);
+  // }
+  // return !letterMap.size;
+
+  // using Dictionaries
+  const dict = {};
 
   for (let char of str1) {
-    letterMap.set(char, letterMap.get(char) + 1 || 1);
+    dict[char] = dict[char] + 1 || 1;
   }
-
   for (let char of str2) {
-    if (!letterMap.has(char)) return false;
-
-    let charVal = letterMap.get(char);
-    if (charVal === 1) letterMap.delete(char);
-    else letterMap.set(char, charVal - 1);
+    if (!(char in dict)) return false;
+    if (dict[char] === 1) delete dict[char];
+    else dict[char]--;
   }
-  return !letterMap.size;
+  return !Object.keys(dict).length;
 };
 
-const str1 = 'abca';
+const str1 = 'baa';
 const str2 = 'aba';
 console.log(checkAnagramOf(str1, str2));

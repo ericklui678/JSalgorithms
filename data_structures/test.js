dc512c581ab77086be6b9647d716f38b17d5881b
Erick Lui <erickjin.lui@gmail.com>
2017-12-12 16:30:38 -0800

    added functionality for paypal sentences

--- data_structures/test.js
+++ data_structures/test.js
@@ -3,8 +3,8 @@ const Trie = require('./trie');
 
 let t1 = new Trie();
 t1.insert('hello');
-t1.insert('hey');
-t1.insert('he');
-t1.insert('play')
-t1.insert('plaza');
+// t1.insert('hey');
+// t1.insert('he');
+// t1.insert('play')
+// t1.insert('plaza');
 console.log(util.inspect(t1, false, null));
--- payPalSentences.js
+++ payPalSentences.js
@@ -1,5 +1,22 @@
-function possibleSentences(str) {
-  const wordMap = {
+/*
+Given the string 'paymentsandpaypal' and a dictionary of predefined valid words, find all possible valid sentences. Valid sentences are defined as valid words separated by other valid words.
+
+EX:
+'paymentsandpaypal' returns
+
+'Payment sand Pay Pal'
+'Payment sand PayPal'
+'Payments and Pay Pal'
+'Payments and PayPal'
+*/
+
+const possibleSentences = (str, result = []) => {
+  // const wordDict = {
+  //   'pay': 1,
+  //   'paypal': 1,
+  //   'pal': 1,
+  // };
+  const wordDict = {
     'a': 1,
     'an': 1,
     'and': 1,
@@ -12,17 +29,44 @@ function possibleSentences(str) {
     'payments': 1,
     'sand': 1,
   };
-}
+  for (let i = 0; i < str.length; i++) {
+    let substr = str.substr(0, str.length - i);
+  //   // console.log(substr);
+  //   if (substr in wordDict) {
+  //     let remainingStr = str.substr(substr.length);
+  //     if (!remainingStr) {
+  //       result.push([substr]);
+  //       console.log('result', result);
+  //     }
+  //     console.log(`substr: ${substr} --- remain: ${remainingStr} --- str: ${str}`);
+  //
+  //     let combinations = possibleSentences(remainingStr);
+      // console.log(`combinations: ${combinations}`);
+    // }
+    if (substr in wordDict) {
+      console.log(`substr: '${substr}' str: '${str}'`);
+      let remainingStr = str.substr(substr.length);
+      if (!remainingStr) {
+        result.push([substr]);
+      } else {
 
-const sentence = 'paymentsandpaypal';
-console.log(possibleSentences(sentence));
+        let SA2 = possibleSentences(remainingStr);
+        let SA = [];
+        if (SA2) {
+          for (let j = 0; j < SA2.length; j++) {
+            SA = [substr];
+            for (let k = 0; k < SA2[j].length; k++) {
+              SA.push(SA2[j][k])
+            }
+            result.push(SA)
+          }
+        }
+      }
+    }
+  }
+  if (!result.length) return false;
+  return result;
+};
 
-
-
-/*
-'Payment sand Pay Pal'
-'Payment sand PayPal'
-
-'Payments and Pay Pal'
-'Payments and PayPal'
-*/
+const str = 'paymentsandpaypal';
+console.log(possibleSentences(str));

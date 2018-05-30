8f965b341aeb3006da22d1f419bcabd182ef9fad
Erick Lui <erickjin.lui@gmail.com>
2018-05-30 14:30:56 -0700

    updated rotateArr.js

--- rotateArray.js
+++ rotateArray.js
@@ -8,44 +8,19 @@
 //   In: [1,2,3,4,5], n = -1
 //   Out: [2,3,4,5,1]
 
-
-const rotateArrayInPlace = (arr, k) => {
-  let rotateBy = k % arr.length;
-  if (rotateBy === 0) return arr;
-
-  arr.reverse();
-  // reverse first half
-  for (let i = 0; i < Math.floor(k/2); i++) {
-    let lastIdx = k - i - 1;
-    [arr[i], arr[lastIdx]] = [arr[lastIdx], arr[i]];
-  }
-  // reverse second half
-  for (let i = 0; i < Math.floor((arr.length - k)/2); i++) {
-    let firstIdx = i + k;
-    let lastIdx = arr.length - i - 1;
-    [[arr[firstIdx]], [arr[lastIdx]]] = [[arr[lastIdx]], [arr[firstIdx]]];
-  }
+const rotateArr = (arr, d) => {
+  const reverseArr = (arr, start, end) => {
+    let numSwaps = Math.floor((end - start) / 2);
+    for (let i = 0; i < numSwaps; i++) {
+      [[arr[start + i]], [arr[end - i]]] = [[arr[end - i]], [arr[start + i]]];
+    }
+    return arr;
+  };
+  reverseArr(arr, 0, arr.length - 1);
+  reverseArr(arr, 0, d - 1);
+  reverseArr(arr, d, arr.length - 1);
   return arr;
 };
 
-/*
-Space Complexity: O(n) where n is the size of the array
-Time Complexity: O(n) since arr splice has to iteratively copy the array
-*/
-const rotateArrayWithSpace = (arr, k) => {
-  let rotateBy = k % arr.length;
-  if (rotateBy === 0) return arr;
-
-  let newArr = arr.slice(arr.length - rotateBy);
-  for (let i = 0; i < rotateBy; i++) {
-    arr.unshift(newArr.pop());
-    arr.pop();
-  }
-  console.log(arr);
-};
-
-let arr = [1,2,3,4,5,6];
-let k = 5;
-
-let newArr = rotateArrayInPlace(arr, k);
-console.log(newArr);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(rotateArr(arr, 1));
